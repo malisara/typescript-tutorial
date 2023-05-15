857db67fd636aff0106ae72ef1ad4962a71bb508
Sara Mali <mali.sara7@gmail.com>
2023-05-15 18:42:26 +0200

    navbar I

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
+import Navbar from "./scenes/navbar"
+
+
 function App() {
-
+  const [selectedPage, setSelectedPage] = useState<string>('home');
   return (
-   
-      <div className="app">
-       app
+      <div className="app bg-gray-20">
+        <Navbar 
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+            />
       </div>
-
   )
 }
 
